2c90b3ab4d073c58108a1a92a3a40b050054b287
jan <ivan.stsepaniuk@gmail.com>
2025-03-05 12:40:04 +0100

    -changed structure -implemented api directory with dedicated services

---
+++ src/api/auth/authHandler.js
@@ -0,0 +1,15 @@
+import { useEffect } from "react";
+import { AuthService } from "./authService";
+
+export const useAuthCodeHandler = () => {
+    useEffect(() => {
+        const urlSearch = new URLSearchParams(window.location.search);
+        const code = urlSearch.get("code");
+
+        if (code !== undefined) {
+            AuthService.sendAuthCode(code)
+                .then((response) => console.log("Auth response:", response))
+                .catch((error) => console.error("Auth error:", error));
+        }
+    }, []);
+};
